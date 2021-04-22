448609e3d051bbacdfb51da19e45ce0bf1ab449e
Vitor Moschetti <vitormoschetti@terra.com.br>
2021-04-22 11:19:48 -0300

    criando classe DAO e somando valores receita, despesa, valor das ações para calcular o saldo. Ajustes no scss

--- projeto_CPDM/src/app/home/home.module.ts
+++ projeto_CPDM/src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { Globals } from './../DAO';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers: [Globals]
 })
 export class HomePageModule {}
