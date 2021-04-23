f503983e1e096437e4532ad4c8bc093c93f05eb9
LGM9797 <monteirolucasgarcia@gmail.com>
2021-04-23 14:30:08 -0300

    Add barra de menu

--- AddTransacoes/src/app/AddTransacoes/home.page.spec.ts
+++ AddTransacoes/src/app/AddTransacoes/home.page.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { HomePage } from './home.page';
+import { HomePage } from './AddTransacoes';
 
 describe('HomePage', () => {
   let component: HomePage;
